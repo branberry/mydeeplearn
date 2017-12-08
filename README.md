81fc65b2f985e3e1ef98ef0556f559b38dd63f12
Brandon <wowbob396@gmail.com>
2017-12-07 21:37:19 -0600

    working on quadratic function with deeplearn methods

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ https://deeplearnjs.org/docs/tutorials/intro.html
 
 ### Chart.js Documentation
 http://www.chartjs.org/docs/latest/getting-started/integration.html
+
+### To run code use 'yarn watch'
--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 import 'babel-polyfill';
 import Chart from 'chart.js';
-import {Array1D, NDArrayMathGPU, Scalar} from 'deeplearn';
+//import './religiondata/global.csv';
+import {Array1D, Array2D, CostReduction, Graph, InCPUMemoryShuffledInputProviderBuilder, NDArray, NDArrayMathGPU, Scalar, Session, SGDOptimizer, Tensor} from 'deeplearn';
 
 /** Runs the example. */
 async function runExample() {
+  const graph = new Graph();
+  // Make a new input in the graph with the shape [] for a scalar
+  const x = graph.placeholder('x',[]);
+
+  // random scalar values for the Graph
+  const a = graph.placeholder('a',[]);
+  const b = graph.placeholder('b',[]);
+  const c = graph.placeholder('c',[]);
+  // creating new tensors to represent the output the operations of the quadratic
+  const order2 = graph.multiply(a,graph.square(x));
+  const order1 = graph.multiply(b,x);
+  const y = graph.add(graph.add(order2,order1), c);
+  // for training we need a label and a cost function
+  const yLabel = graph.placeholder('y label',[]);
+  // providing a mean squared cost function.  cost = (y-ylabel)^2
+  const cost = graph.meanSquaredCost(y,yLabel);
+
+  // the graph is set up, now we must evaluate it
+  // the session object is needed to evaluate the graph
   const math = new NDArrayMathGPU();
-  const a = Array1D.new([1, 2, 3]);
-  const b = Scalar.new(2);
+  const session = new Session(graph,math);
+  await math.scope(async (keep,track) =>{
 
-  const result = math.add(a, b);
+  });
 
-
-  // Option 1: With async/await.
-  // Caveat: in non-Chrome browsers you need to put this in an async function.
-  console.log(await result.data()); // Float32Array([3, 4, 5])
-
-  // Option 2: With a Promise.
-  result.data().then((data) => console.log(data));
-
-  // Option 3: Synchronous download of data.
-  // This is simpler, but blocks the UI until the GPU is done.
-  console.log(result.dataSync());
 }
 
 runExample();
