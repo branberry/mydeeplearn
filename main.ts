42f74069c056da026860692a8db745eb47438442
Brandon <wowbob396@gmail.com>
2017-12-13 21:03:07 -0600

    updated main.ts to work on the complementary color prediction tutorial

--- main.ts
+++ main.ts
@@ -1,21 +1,16 @@
-import {Array1D, NDArrayMathGPU, Scalar} from 'deeplearn';
+/*
+ This file will be trained to predict complementary colors 
+ */
+import {Array1D, CostReduction, FeedEntry, Graph, InCPUMemoryShuffledInputProviderBuilder, NDArrayMath, NDArrayMathGPU, Session, SGDOptimizer, Tensor} from 'deeplearn';
 
-async function runExample() {
-  const math = new NDArrayMathGPU();
-  const a = Array1D.new([1, 2, 3]);
-  const b = Scalar.new(2);
+class ComplementaryColorModel {
+    // runs training
+    session: Session;
 
-  const result = math.add(a, b);
+    //encapsulates math operations on the CPU and GPU.
+    math: NDArrayMath = new NDArrayMathGPU();
 
-  // Option 1: With async/await.
-  // Caveat: in non-Chrome browsers you need to put this in an async function.
-  console.log(await result.data());  // Float32Array([3, 4, 5])
-  // Option 2: With a Promise.
-  result.data().then(data => console.log(data));
-
-  // Option 3: Synchronous download of data.
-  // This is simpler, but blocks the UI until the GPU is done.
-  console.log(result.dataSync());
+    // An optimizer with a certain initial learning rate.  Used for training.
+    intialLearningRate = 0.042;
+    optimizer: SGDOptimizer;
 }
-
-runExample();
